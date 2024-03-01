a5aeba17f9f4db7d3fa4a9ac499970957dfb73e0
Olsen7351 <olsen7351@gmail.com>
2024-03-01 07:04:07 +0100

    Feature: Setup detailspage about pokemons

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,6 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState, useEffect } from "react";
+import { Route, Routes, Link } from "react-router-dom";
+import { Button } from "bootstrap";
+import { useParams } from "react-router-dom";
 
 function SearchBar() {}
 
@@ -44,7 +47,7 @@ function PokemonList() {
               #{pokemonId + index + 1}
               {" " + poke.name.charAt(0).toUpperCase() + poke.name.slice(1)}
             </span>
-            <button className="btn btn-primary">Details</button>
+            <Link to={`/PokemonInfo/${poke.name}`}>More Info</Link>
           </div>
         ))}
       </div>
@@ -57,13 +60,46 @@ function PokemonList() {
   );
 }
 
-function PokemonInfo() {}
+function PokemonInfo(props) {
+  const [pokemon, setPokemon] = useState(null);
+  const { pokemonName } = useParams();
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setPokemon(data);
+      })
+      .catch((err) => console.log(err));
+  }, [pokemonName]);
+
+  return (
+    <div>
+      {pokemon && (
+        <div>
+          <h1>{pokemon.name}</h1>
+          <img
+            src={pokemon.sprites.front_default}
+            alt={pokemon.name}
+            width="200"
+          />
+          <p>Height: {pokemon.height}</p>
+          <p>Weight: {pokemon.weight}</p>
+        </div>
+      )}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <PokemonList></PokemonList>
+        <Routes>
+          <Route path="/PokemonInfo/:pokemonName" element={<PokemonInfo />} />
+          <Route path="/" element={<PokemonList />} />
+        </Routes>
       </header>
     </div>
   );
