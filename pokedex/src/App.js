c1c75158424ea3dcbfe39b6a2e7aaa18a0fe1fcc
Olsen7351 <olsen7351@gmail.com>
2024-03-01 07:26:45 +0100

    Bug: Brake commit - Fetch of pokemons dont work properly the id is incereased each time and picture is changed

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -9,28 +9,30 @@ function SearchBar() {}
 
 function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
-  const [pokemonId, setPokemonId] = useState(0);
+  const [offset, setOffset] = useState(0);
+  const [totalCount, setTotalCount] = useState(0);
 
-  const fetchPokemon = (id) => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/?limit=10&offset=${id}`)
+  const fetchPokemon = () => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/?limit=10&offset=${offset}`)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
+        setTotalCount(data.count);
         setPokemon((prevPokemon) => [...prevPokemon, ...data.results]);
       })
       .catch((err) => console.log(err));
   };
 
   const handleShowMore = () => {
-    const newId = pokemonId + 10;
-    setPokemonId(newId);
-    fetchPokemon(newId);
+    const newOffset = offset + 10;
+    if (newOffset < totalCount) {
+      setOffset(newOffset);
+    }
   };
 
   useEffect(() => {
-    fetchPokemon(pokemonId);
-    console.log("I log once");
-  }, []);
+    fetchPokemon();
+  }, [offset]); // Trigger fetchPokemon whenever offset changes
 
   return (
     <div>
@@ -39,22 +41,24 @@ function PokemonList() {
           <div key={index} className="pokemon-circle">
             <img
               src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-                pokemonId + index + 1
+                index + offset + 1
               }.png`}
               alt={poke.name}
             />
             <span>
-              #{pokemonId + index + 1}
-              {" " + poke.name.charAt(0).toUpperCase() + poke.name.slice(1)}
+              #{index + offset + 1}{" "}
+              {poke.name.charAt(0).toUpperCase() + poke.name.slice(1)}
             </span>
             <Link to={`/PokemonInfo/${poke.name}`}>More Info</Link>
           </div>
         ))}
       </div>
       <div className="text-center">
-        <button onClick={handleShowMore} className="btn btn-primary">
-          Show More
-        </button>
+        {offset + 10 < totalCount && (
+          <button onClick={handleShowMore} className="btn btn-primary">
+            Show More
+          </button>
+        )}
       </div>
     </div>
   );
